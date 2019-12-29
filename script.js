92f83b38f14e4f33801b07f1a1303fb93f904356
Salko Hajrić <38767078+mewtwo0719@users.noreply.github.com>
2019-12-29 18:19:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,21 +34,21 @@ function Snowflake(x, y, r, c, t, v){
 }
 
 var layer1 = [];
-var snowflakes1 = 150;
+var snowflakes1 = 60;
 
 for(var i = 0; i <= snowflakes1; i++){
     layer1.push(new Snowflake((Math.random()*canvas.width), (-Math.random()*canvas.height*2 + canvas.height), (Math.random()*1+2), "white", (Math.random()*100 + 100), 0));
 }
 
 var layer2 = [];
-var snowflakes2 = 550;
+var snowflakes2 = 300;
 
 for(var i = 0; i <= snowflakes2; i++){
     layer2.push(new Snowflake(((Math.random()*canvas.width*1.5) - canvas.width/2), (-Math.random()*canvas.height*2 + canvas.height), (Math.random()*1+1), "white", -1, .3));
 }
 
 var layer3 = [];
-var snowflakes3 = 1050;
+var snowflakes3 = 600;
 
 for(var i = 0; i <= snowflakes3; i++){
     layer3.push(new Snowflake(((Math.random()*canvas.width*2)), (-Math.random()*canvas.height*2 + canvas.height), (Math.random()*1+.5), "white", -1, -.3));
